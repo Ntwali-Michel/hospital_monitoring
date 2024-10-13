e823f7fa8d8c9df3d3db091c220341e848325309
Mike Loen <m.ntwali@alustudent.com>
2024-10-13 11:31:58 +0200

    backup_archives.sh

---
+++ backup_archives.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+group_number=1  # Replace with your actual group number
+
+mkdir -p archived_logs_group$group_number
+mv heart_rate_log.txt_* archived_logs_group$group_number/
+scp archived_logs_group$group_number/* username@hostname:/home/
+echo "Backup completed and archived logs moved to archived_logs_group$group_number"
+
